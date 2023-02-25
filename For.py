5bb823fe6a4d1be0168a14f468a3015898dadee3
unknown <airas.latypov@gmail.com>
2023-02-25 11:04:27 -0800

    While

---
+++ For.py
@@ -0,0 +1,12 @@
+y_start = int(input())
+y_finish = int(input())
+x_start = int(input())
+x_finish = int(input())
+
+
+line_x = []
+
+for x in range(x_start, x_finish+1):
+    line_x.append(x)
+
+print(line_x)
